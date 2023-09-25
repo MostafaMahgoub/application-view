c719bf3bc82da82731b8b1b99e65b590fbb6bca3
MostafaR <mostafamahgoub@outlook.com>
2023-09-25 19:20:25 +0300

    Initialize the app-view project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      fontSize: {
+        '2xl': '1.2rem',
+      },
+    },
+  },
+  plugins: [],
+};
