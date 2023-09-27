58a6db4c56f25dcfde0bf55b975907103e4f780a
MostafaR <mostafamahgoub@outlook.com>
2023-09-27 16:43:11 +0300

    Apply clean code concepts to the table

--- src/Content/Components/Table.tsx
+++ src/Content/Components/Table.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Divider, Table } from "antd";
+import { Table } from "antd";
 import CandidateCard from "./CandidateCard";
-import jsonData from '../candidateData.json';
+import candidateData from '../candidateData.json';
+import TableHeader from "./TableHeader";
 
 interface Candidate {
   name: string;
@@ -14,61 +15,38 @@ interface DataItem {
   Candidate: JSX.Element;
 }
 
-const columns = [
+interface TableColumns {
+  title: JSX.Element;
+  dataIndex: string;
+  render: (Candidate: JSX.Element) => JSX.Element;
+}
+
+const tableColumns: TableColumns[] = [
   {
-    title: (
-      <div className="flex flex-row">
-        <span className="text-[#1D4ED8] text-sm font-semibold font-poppins leading-6 break-words">
-          247 Candidates
-        </span>
-        <div className="flex-grow" />
-        <span className="text-[#1D4ED8] text-sm font-medium font-poppins leading-6 break-words">
-          Qualified
-        </span>
-        <Divider type="vertical" className="self-center" />
-        <div className="flex gap-2">
-          <span className="text-[#0B0B0B] text-sm font-normal font-poppins leading-6 break-words">
-            Task
-          </span>
-          <div className="px-0.3 py-0.1 h-fit bg-gray-200 rounded-xl flex justify-center items-center self-center gap-10">
-            <div className="text-gray-700 text-xs font-poppins font-medium break-words">
-              25
-            </div>
-          </div>
-        </div>
-        <Divider type="vertical" className="self-center" />
-        <div className="flex gap-2">
-          <span className="text-[#0B0B0B] text-sm font-normal font-poppins leading-6 break-words">
-            Disqualified
-          </span>
-          <div className="px-0.3 py-0.1 h-fit bg-gray-200 rounded-xl flex justify-center items-center self-center gap-10">
-            <div className="text-gray-700 text-xs font-poppins font-medium break-words">
-              25
-            </div>
-          </div>
-        </div>
-      </div>
-    ),
+    title: <TableHeader />,
     dataIndex: "Candidate",
     render: (Candidate: JSX.Element) => <a>{Candidate}</a>,
   },
 ];
 
-const candidates: Candidate[] = jsonData;
+const candidates: Candidate[] = candidateData;
 
+function mapCandidatesToDataItems(candidates: Candidate[]): DataItem[] {
+  return candidates.map((candidate, index) => {
+    return {
+      key: String(index + 1),
+      Candidate: (
+        <CandidateCard
+          name={candidate.name}
+          country={candidate.country}
+          education={candidate.education}
+        />
+      ),
+    };
+  });
+}
 
-const data: DataItem[] = candidates.map((candidate, index) => {
-  return {
-    key: String(index + 1),
-    Candidate: (
-      <CandidateCard
-        name={candidate.name}
-        country={candidate.country}
-        education={candidate.education}
-      />
-    ),
-  };
-});
+const data: DataItem[] = mapCandidatesToDataItems(candidates);
 
 const rowSelection = {
   onChange: (selectedRowKeys: React.Key[], selectedRows: DataItem[]) => {
@@ -85,7 +63,7 @@ function CandidatesTable() {
           type: "checkbox",
           ...rowSelection,
         }}
-        columns={columns}
+        columns={tableColumns}
         dataSource={data}
         pagination={false}
       />
---
+++ src/Content/Components/TableHeader.tsx
@@ -0,0 +1,39 @@
+import { Divider } from "antd";
+
+
+function TableHeader(){
+    return(
+        <div className="flex flex-row">
+        <span className="text-[#1D4ED8] text-sm font-semibold font-poppins leading-6 break-words">
+          247 Candidates
+        </span>
+        <div className="flex-grow" />
+        <span className="text-[#1D4ED8] text-sm font-medium font-poppins leading-6 break-words">
+          Qualified
+        </span>
+        <Divider type="vertical" className="self-center" />
+        <div className="flex gap-2">
+          <span className="text-[#0B0B0B] text-sm font-normal font-poppins leading-6 break-words">
+            Task
+          </span>
+          <div className="px-0.3 py-0.1 h-fit bg-gray-200 rounded-xl flex justify-center items-center self-center gap-10">
+            <div className="text-gray-700 text-xs font-poppins font-medium break-words">
+              25
+            </div>
+          </div>
+        </div>
+        <Divider type="vertical" className="self-center" />
+        <div className="flex gap-2">
+          <span className="text-[#0B0B0B] text-sm font-normal font-poppins leading-6 break-words">
+            Disqualified
+          </span>
+          <div className="px-0.3 py-0.1 h-fit bg-gray-200 rounded-xl flex justify-center items-center self-center gap-10">
+            <div className="text-gray-700 text-xs font-poppins font-medium break-words">
+              25
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+}
+export default TableHeader;
