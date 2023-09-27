2995ea2247f004bda24ee4dc86da5525e905656d
MostafaR <mostafamahgoub@outlook.com>
2023-09-27 16:13:28 +0300

    Apply clean code concepts to the CandidateCard

--- src/Content/Components/CandidateCard.tsx
+++ src/Content/Components/CandidateCard.tsx
@@ -1,46 +1,74 @@
-import { Avatar , Tag } from "antd";
+import { Avatar, Tag } from "antd";
+
+interface TextProps {
+  children: React.ReactNode;
+}
+
+function TextBase({ children }: TextProps) {
+  return (
+    <div className="text-black text-base font-poppins font-semibold break-words">
+      {children}
+    </div>
+  );
+}
+
+function TextSmall({ children }: TextProps) {
+  return (
+    <div className="text-black text-xs font-poppins font-medium break-words">
+      {children}
+    </div>
+  );
+}
+
+function TextSmallLight({ children }: TextProps) {
+  return (
+    <div className="text-black text-xs font-poppins font-light break-words">
+      {children}
+    </div>
+  );
+}
+
+function TextSmallNormal({ children }: TextProps) {
+  return (
+    <div className="text-[#1D4ED8] text-xs font-poppins font-normal break-words">
+      {children}
+    </div>
+  );
+}
+
+function TagCard({ children }: TextProps) {
+  return (
+    <Tag bordered={false} className="rounded-xl bg-[#EDF4FF]">
+      <div className="text-[#037092] text-xs font-poppins font-medium break-words">
+        {children}
+      </div>
+    </Tag>
+  );
+}
 
 function CandidateCard() {
   return (
-    <div className="flex flex-row gap-5">
-      <Avatar className="align-middle bg-[#EDF4FF] text-[#D0E1FF] self-center" size="large">
+    <div className="flex flex-row gap-8">
+      <Avatar
+        className="align-middle bg-[#EDF4FF] text-[#D0E1FF] self-center"
+        size="large"
+      >
         MR
       </Avatar>
       <div className="flex flex-col gap-2">
-        <div className="text-black text-base font-poppins font-semibold break-words">
-          Aaliyah Sanderson
-        </div>
-        <div className="text-black text-xs font-poppins font-medium break-words">
-          Riyadh, Saudi Arabia
-        </div>
-        <div className="text-black text-xs font-poppins font-light break-words">
+        <TextBase>Aaliyah Sanderson</TextBase>
+        <TextSmall>Riyadh, Saudi Arabia</TextSmall>
+        <TextSmallLight>
           Bachelor - Cambridge University (2023 - 2023)
+        </TextSmallLight>
+        <div className="flex flex-row gap-2">
+          <TextSmallNormal>#top_candidate</TextSmallNormal>
+          <TextSmallNormal>#top_candidate</TextSmallNormal>
         </div>
         <div className="flex flex-row gap-2">
-        <div className="text-[#1D4ED8] text-xs font-poppins font-normal break-words">
-  #top_candidate
-</div>
-<div className="text-[#1D4ED8] text-xs font-poppins font-normal break-words">
-  #top_candidate
-</div>
-        </div>
-        <div className="flex flex-row gap-2">
-        <Tag bordered={false} className="rounded-xl bg-[#EDF4FF]">
-        <div className="text-[#037092] text-xs font-poppins font-medium break-words">
-  New York
-</div>
-      </Tag>
-      <Tag bordered={false} className="rounded-xl bg-[#EDF4FF]">
-        <div className="text-[#037092] text-xs font-poppins font-medium break-words">
-  Marketing
-</div>
-      </Tag>
-      <Tag bordered={false} className="rounded-xl bg-[#EDF4FF]">
-        <div className="text-[#037092] text-xs font-poppins font-medium break-words">
-  London
-</div>
-      </Tag>
-      
+          <TagCard>New York</TagCard>
+          <TagCard>Marketing</TagCard>
+          <TagCard>London</TagCard>
         </div>
       </div>
     </div>
